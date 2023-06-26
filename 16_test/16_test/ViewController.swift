e52f8e62efb7443ab3c6207be6f5a7eabaea3a61
coderHDY <huangdeyu2020@163.com>
2023-06-26 21:00:46 +0800

    16test

--- 16_test/16_test/ViewController.swift
+++ 16_test/16_test/ViewController.swift
@@ -8,12 +8,14 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var textLabel: UILabel!
+    @IBOutlet weak var btn: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        print(textLabel.text ?? "");
+        textLabel.text = "coderhdy\n好好学习"
     }
-
-
 }
 
